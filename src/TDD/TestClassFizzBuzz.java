fdf786c769d785da699d6f2add5778cc08d6f479
papoo25666 <papoo25666@gmail.com>
2019-10-03 15:58:47 +0700

    Create Java Calss: FizzBuzz change mathod name

--- src/TDD/TestClassFizzBuzz.java
+++ src/TDD/TestClassFizzBuzz.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TestClassFizzBuzz {
 
 	@Test
-	void test_input_expect_output_is_1() {
+	void test_input_is_1_expect_output_is_1() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
 		int input = fizzbuzz.printStringinput(1);
 		assertEquals(1, input);
