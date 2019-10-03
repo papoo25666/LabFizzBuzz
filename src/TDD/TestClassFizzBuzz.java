92a985f567f0dabf39543034c2973c18243f6458
papoo25666 <papoo25666@gmail.com>
2019-10-03 16:03:49 +0700

    Create Java Calss: FizzBuzz change mathod name

--- src/TDD/TestClassFizzBuzz.java
+++ src/TDD/TestClassFizzBuzz.java
@@ -9,7 +9,7 @@ class TestClassFizzBuzz {
 	@Test
 	void test_input_is_1_expect_output_is_1() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
-		int input = fizzbuzz.printStringinput(1);
+		int input = fizzbuzz.printStringinput (1);
 		assertEquals(1, input);
 	}
 
