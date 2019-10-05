dd79556017de17852cc54b51f58fd7beaad72ec7
tanagon2910 <tanagon2910@hotmail.com>
2019-10-05 19:19:41 +0700

    FizzBuzz V.8 input 8

--- FizzBuzz/src/TDD/TestClassFizzBuzz.java
+++ FizzBuzz/src/TDD/TestClassFizzBuzz.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 class TestClassFizzBuzz {
 
 	@Test
-	void test_input_is_7_expect_output_is_7() {
+	void test_input_is_8_expect_output_is_8() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
-		String input = fizzbuzz.printStringinput(7);
-		assertEquals("7", input);
+		String input = fizzbuzz.printStringinput(8);
+		assertEquals("8", input);
 	}
 
 }
