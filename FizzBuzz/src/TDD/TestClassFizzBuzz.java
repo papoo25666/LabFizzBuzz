a6f3c8b6a238483494538e119bef0ed5d4b85019
papoo25666 <papoo25666@gmail.com>
2019-10-05 19:03:59 +0700

    FizzBuzz V.5 input 5 refactor method name

--- FizzBuzz/src/TDD/TestClassFizzBuzz.java
+++ FizzBuzz/src/TDD/TestClassFizzBuzz.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TestClassFizzBuzz {
 
 	@Test
-	void test_input_is_4_expect_output_is_4() {
+	void test_input_is_5_expect_output_is_Buzz() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
 		String input = fizzbuzz.printStringinput(5);
 		assertEquals("Buzz", input);
