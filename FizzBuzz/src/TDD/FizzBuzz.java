dbba758118b878f55a10957671cf6352a06aedaa
papoo25666 <papoo25666@gmail.com>
2019-10-08 17:19:49 +0700

    FizzBuzz V.15 edit testcase

--- FizzBuzz/src/TDD/FizzBuzz.java
+++ FizzBuzz/src/TDD/FizzBuzz.java
@@ -4,16 +4,15 @@ public class FizzBuzz {
 
 	public String printStringinput(int number) {
 		String result = Integer.toString(number);
+		
 		if(number%15 == 0) {
 			result ="FizzBuzz";
-		}else
-		if(number%3 == 0) {
+		}else if(number%3 == 0) {
 			result = "Fizz";
 		}else if(number%5 == 0){
 			result = "Buzz";
 		}
 		return result;
-		
 	}
-	}
+}
 
--- FizzBuzz/src/TDD/TestClassFizzBuzz.java
+++ FizzBuzz/src/TDD/TestClassFizzBuzz.java
@@ -7,10 +7,94 @@ import org.junit.jupiter.api.Test;
 class TestClassFizzBuzz {
 
 	@Test
+	void test_input_is_1_expect_output_is_1() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(1);
+		assertEquals("1", input);
+	}
+	@Test
+	void test_input_is_2_expect_output_is_2() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(2);
+		assertEquals("2", input);
+	}
+	@Test
+	void test_input_is_3_expect_output_is_Fizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(3);
+		assertEquals("Fizz", input);
+	}
+	@Test
+	void test_input_is_4_expect_output_is_4() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(4);
+		assertEquals("4", input);
+	}
+	@Test
+	void test_input_is_5_expect_output_is_Buzz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(5);
+		assertEquals("Buzz", input);
+	}
+	@Test
+	void test_input_is_6_expect_output_is_Fizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(6);
+		assertEquals("Fizz", input);
+	}
+	@Test
+	void test_input_is_7_expect_output_is_7() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(7);
+		assertEquals("7", input);
+	}
+	@Test
+	void test_input_is_8_expect_output_is_8() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(8);
+		assertEquals("8", input);
+	}
+	@Test
+	void test_input_is_9_expect_output_is_Fizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(9);
+		assertEquals("Fizz", input);
+	}
+	@Test
+	void test_input_is_10_expect_output_is_Buzz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(10);
+		assertEquals("Buzz", input);
+	}
+	@Test
+	void test_input_is_11_expect_output_is_11() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(11);
+		assertEquals("11", input);
+	}
+	@Test
+	void test_input_is_12_expect_output_is_Fizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(12);
+		assertEquals("Fizz", input);
+	}
+	@Test
+	void test_input_is_13_expect_output_is_13() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(13);
+		assertEquals("13", input);
+	}
+	@Test
+	void test_input_is_14_expect_output_is_14() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String input = fizzbuzz.printStringinput(14);
+		assertEquals("14", input);
+	}
+	@Test
 	void test_input_is_15_expect_output_is_FizzBuzz() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
-		String input = fizzbuzz.printStringinput(15);
-		assertEquals("FizzBuzz", input);
+		String input = fizzbuzz.printStringinput(3);
+		assertEquals("Fizz", input);
 	}
 
 }
